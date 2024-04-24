149a3c887dc594dcce03059212b3391b9937b7b2
nojus <me@nojus.dev>
2024-04-24 06:19:10 +0300

    Initial commit

---
+++ datapacker.py
@@ -0,0 +1,139 @@
+import struct
+import ubinascii
+
+structure = {
+    "time": {
+        # Time in ms since start of day
+        "type": "unsigned int",
+        "size": 4,
+        "multiplier": 1
+    },
+
+    "lat": {
+        "type": "int",
+        "size": 4,
+        # Scale down to 7 decimal places, should be enough
+        "multiplier": 10 ** 7
+    },
+    "lon": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 10 ** 7
+    },
+    "gps_sats": {
+        "type": "unsigned short",
+        "size": 2,
+        "multiplier": 1
+    },
+
+    "baro_bmp": {
+        "type": "unsigned int",
+        "size": 4,
+        "multiplier": 10000
+    },
+    "baro_bme": {
+        "type": "unsigned int",
+        "size": 4,
+        "multiplier": 10000
+    },
+    "baro_ms5611": {
+        "type": "unsigned int",
+        "size": 4,
+        "multiplier": 10000
+    },
+
+    "temp_bmp": {
+        "type": "short",
+        "size": 4,
+        "multiplier": 100
+    },
+    "temp_mpu": {
+        "type": "short",
+        "size": 4,
+        "multiplier": 100
+    },
+    "temp_bme": {
+        "type": "short",
+        "size": 4,
+        "multiplier": 100
+    },
+    "temp_ms5611": {
+        "type": "short",
+        "size": 4,
+        "multiplier": 100
+    },
+
+    "hum_bme": {
+        "type": "short",
+        "size": 4,
+        "multiplier": 100
+    },
+
+    "acc_x": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+    "acc_y": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+    "acc_z": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+
+    "gyro_x": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+    "gyro_y": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+    "gyro_z": {
+        "type": "int",
+        "size": 4,
+        "multiplier": 1000
+    },
+
+
+}
+
+types = {
+    "int": "i",
+    "unsigned int": "I",
+    "short": "h",
+    "unsigned short": "H"
+}
+
+size = sum([structure[i]["size"] for i in structure])  # 44 bytes == 352 bits\
+
+def set_value(value, key):
+    structure[key]["value"] = value * structure[key]["multiplier"]
+
+
+def set_values(values):
+    for key in values:
+        set_value(values[key], key)
+
+
+def pack():
+    fmt = ">" + "".join([types[structure[i]["type"]] for i in structure])
+    s = struct.pack(fmt, *[int(structure[i].get("value", 0) * structure[i]["multiplier"]) for i in structure])
+    # B64
+    return ubinascii.b2a_base64(s).decode("utf-8")
+
+
+def unpack(data):
+    # Return a dictionary with the values
+    fmt = ">" + "".join([types[structure[i]["type"]] for i in structure])
+    ub = ubinascii.a2b_base64(data)
+    unpacked = struct.unpack(fmt, data)
+
+    return {list(structure.keys())[i]: unpacked[i] / list(structure.values())[i]["multiplier"] for i in
+            range(len(structure))}
---
+++ main.py
@@ -0,0 +1,144 @@
+import _thread
+import os
+
+import machine
+
+import datapacker
+import lora_e220_constants
+from hardware import lora_e220, micropyGPS, bmp280, bme280, ms5611, sim800l
+from hardware.imu import MPU6050
+
+######## GENERAL SETUP  #########
+
+rtc = machine.RTC()
+sd = machine.SDCard(slot=2, miso=18, mosi=21, sck=8, cs=9, freq=2_000_000)
+
+os.mount(sd, "/sd")
+
+print(os.listdir("/sd"))
+
+########## I2C SETUP ############
+i2c_main = machine.SoftI2C(scl=machine.Pin(3), sda=machine.Pin(5))
+print([i for i in i2c_main.scan()])  # ['0x68' -- 6500, '0x76' -- bmp, '0x77' -- ms5611]
+
+i2c_secondary = machine.SoftI2C(scl=machine.Pin(11), sda=machine.Pin(12))  # todo
+print([i for i in i2c_secondary.scan()])  # ['0x76' -- bme280]
+
+###########  GSM SETUP   ############
+gsm = sim800l.Modem(  # todo
+    uart=machine.UART(2, tx=machine.Pin(17), rx=machine.Pin(16))
+)
+
+gsm.initialize()
+gsm.connect('internet.tele2.lt')
+
+
+def gsm_loop():
+    while True:
+        print(gsm.get_ip_addr())
+        print(gsm.get_info())
+        print(gsm.get_signal_strength())
+
+        gsm.http_request("http://sat.xe1h.xyz/sat/gsm", "POST", datapacker.pack(), "text/plain")
+
+
+########### SENSOR SETUP ############
+mpu = MPU6050(i2c_main, device_addr=0)
+bmp = bmp280.BMP280(i2c_main)
+ms5611 = ms5611.MS5611(i2c_main)
+
+bme = bme280.BME280(i2c_secondary)
+
+
+########### RADIO SETUP  ############
+radio = lora_e220.LoRaE220('400T22D', machine.UART(1, tx=machine.Pin(33), rx=machine.Pin(34)))
+
+radio_configuration = lora_e220.Configuration("400T22D")
+
+radio_configuration.CHAN = 24
+radio_configuration.TRANSMISSION_MODE.enableRSSI = lora_e220_constants.RssiEnableByte.RSSI_ENABLED
+
+radio.set_configuration(radio_configuration)
+
+radio.begin()
+
+
+def radio_loop():
+    while True:
+        dp = datapacker.pack()
+        code = radio.send_transparent_message(dp)
+
+
+###########  GPS SETUP   ############
+
+gps = machine.UART(0, baudrate=9600, tx=machine.Pin(37), rx=machine.Pin(38))
+gps_parser = micropyGPS.MicropyGPS(local_offset=3, location_formatting="dd")  # offset for UTC+3, decimal degrees
+
+# Run radio loop in a separate thread to not block the main loop,
+# use main loop only for sampling sensors and saving onboard
+_thread.start_new_thread(radio_loop, ())
+_thread.start_new_thread(gsm_loop, ())
+
+while True:
+    accel = mpu.accel.xyz
+    gyro = mpu.gyro.xyz
+    bme_temp, bme_pressure, bme_hum = bme.values
+    ms5611_temp, ms5611_pressure = ms5611.measurements
+
+    # millis since start of day
+    # rtc.datetime() returns a tuple with the following format:
+    # (year, month, day[, hour[, minute[, second[, microsecond)
+
+    millis = int(
+        rtc.datetime()[3] * 3600000 + rtc.datetime()[4] * 60000 + rtc.datetime()[5] * 1000 + rtc.datetime()[6] // 1000)
+
+    d = {
+        "time": millis,
+
+        "temp_bmp": bmp.temperature,
+        "temp_mpu": mpu.temperature,
+        "temp_bme": bme_temp,
+        "temp_ms5611": ms5611_temp,
+
+        "hum_bme": bme_hum,
+
+        "baro_bmp": bmp.pressure,
+        "baro_bme": bme_pressure,
+        "baro_ms5611": ms5611_pressure,
+
+        "acc_x": accel[0],
+        "acc_y": accel[1],
+        "acc_z": accel[2],
+
+        "gyro_x": gyro[0],
+        "gyro_y": gyro[1],
+        "gyro_z": gyro[2]
+    }
+
+    if gps.any():
+        line = gps.readline()
+
+        for c in line:
+            gps_parser.update(chr(c))
+
+        d["lat"] = gps_parser.latitude[0]
+        d["lon"] = gps_parser.longitude[0]
+
+        seconds = gps_parser.timestamp[2]
+        # Seconds is a float, convert to seconds int and microseconds int
+        microseconds = int((seconds - int(seconds)) * 1000000)
+        seconds = int(seconds)
+
+        current_dt = (gps_parser.date[2], gps_parser.date[1], gps_parser.date[0], gps_parser.timestamp[0],
+                      gps_parser.timestamp[1], seconds, microseconds)
+
+        # Set the RTC time to the GPS time
+        rtc.datetime(current_dt)
+
+        d["gps_sats"] = gps_parser.satellites_in_use
+
+    datapacker.set_values(d)
+
+    # Save data to the SD card
+    with open("sd/data.txt", "a") as f:
+        f.write(datapacker.pack() + "\n")
